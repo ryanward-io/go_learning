b39faff8c79f33d638f68619997b5c204f3244c0
Ryan Ward <ryan@fromlawtodata.com>
2022-07-28 00:57:35 +1000

    Working through basic Go tutorial

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Ryan", "Darryl", "Jemima", "Erika", "Graham"}
+
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
+}
